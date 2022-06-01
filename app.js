25d1d45dccbb753a01ce4cc584c1a8c4292e2bf8
CaptDevil <prabhudutta.sarangi@gmail.com>
2022-06-01 21:53:46 +0530

    created backend server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res) => {
+    
+})
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server started on port ${port}...`))
